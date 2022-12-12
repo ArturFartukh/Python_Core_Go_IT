1b97d2e79aab48531c48981b70aef8a41617b020
Artur Fartukh <ar4ik.8933@gmail.com>
2022-12-12 12:01:17 +0200

    Create go_it-python_core_hw12_05.py

---
+++ go_it-python_core_hw12/go_it-python_core_hw12_05.py
@@ -0,0 +1,35 @@
+import pickle
+
+
+class Person:
+    def __init__(self, name: str, email: str, phone: str, favorite: bool):
+        self.name = name
+        self.email = email
+        self.phone = phone
+        self.favorite = favorite
+
+
+class Contacts:
+    count_save = 0
+
+    def __init__(self, filename: str, contacts: list[Person] = None):
+        if contacts is None:
+            contacts = []
+        self.filename = filename
+        self.contacts = contacts
+        self.count_save = 0
+
+    def save_to_file(self):
+        with open(self.filename, "wb") as file:
+            pickle.dump(self, file)
+
+    def read_from_file(self):
+        with open(self.filename, "rb") as file:
+            content = pickle.load(file)
+        return content
+
+    def __getstate__(self):
+        attributes = self.__dict__.copy()
+        attributes['count_save'] += 1
+
+        return attributes
