afff47ebe9ff3c7df21de36cf7ecb8b62604159d
Artur Fartukh <ar4ik.8933@gmail.com>
2022-12-13 10:03:24 +0200

    Create go_it-python_core_hw12_08.py

---
+++ go_it-python_core_hw12/go_it-python_core_hw12_08.py
@@ -0,0 +1,46 @@
+import copy
+import pickle
+
+
+class Person:
+    def __init__(self, name: str, email: str, phone: str, favorite: bool):
+        self.name = name
+        self.email = email
+        self.phone = phone
+        self.favorite = favorite
+
+
+def copy_class_person(person):
+    return copy.copy(person)
+
+
+class Contacts:
+    def __init__(self, filename: str, contacts: list[Person] = None):
+        if contacts is None:
+            contacts = []
+        self.filename = filename
+        self.contacts = contacts
+        self.is_unpacking = False
+        self.count_save = 0
+
+    def save_to_file(self):
+        with open(self.filename, "wb") as file:
+            pickle.dump(self, file)
+
+    def read_from_file(self):
+        with open(self.filename, "rb") as file:
+            content = pickle.load(file)
+        return content
+
+    def __getstate__(self):
+        attributes = self.__dict__.copy()
+        attributes["count_save"] = attributes["count_save"] + 1
+        return attributes
+
+    def __setstate__(self, value):
+        self.__dict__ = value
+        self.is_unpacking = True
+
+
+def copy_class_contacts(contacts):
+    return copy.deepcopy(contacts)
