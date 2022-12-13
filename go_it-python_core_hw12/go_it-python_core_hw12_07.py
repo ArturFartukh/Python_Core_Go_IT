ae898b2629a14762862d8c83af52603bbec9a139
Artur Fartukh <ar4ik.8933@gmail.com>
2022-12-13 09:56:38 +0200

    Create go_it-python_core_hw12_07.py

---
+++ go_it-python_core_hw12/go_it-python_core_hw12_07.py
@@ -0,0 +1,13 @@
+import copy
+
+
+class Person:
+    def __init__(self, name: str, email: str, phone: str, favorite: bool):
+        self.name = name
+        self.email = email
+        self.phone = phone
+        self.favorite = favorite
+
+
+def copy_class_person(person):
+    return copy.copy(person)
